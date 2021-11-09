74d686026b6db73fb6a6fa7ec3eda054c12f8b5f
Djalabi Mahamat Abakar <93582720+Djalabi@users.noreply.github.com>
2021-11-09 09:39:37 +0100

    fichier 2.c

---
+++ fichier 2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Saisissez successivement deux entiers : ");
+    scanf("%d %d",&a,&b);
+    printf("\n %d + %d = %d",a,b,(a+b));
+    return 0;
+}
