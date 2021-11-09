2b3d3e1b0168be47cfb6d896207fa1d58cb83226
Djalabi <djalabi97@gmail.com>
2021-11-09 09:08:01 +0100

    On est entrain de faire des opérartions sur les deux fichiers modifiés

--- fichier 1.c
+++ fichier 1.c
@@ -2,6 +2,9 @@
 
 int main()
 {
-    printf("Hello World !");
+    printf("Salut petit !\n");
+    printf("Coment allez-vous ?\n");
+    printf("Je sais que vous etes ingénieur des données pas possible, merci. \n");
+
     return 0;
 }
--- fichier 2.c
+++ fichier 2.c
@@ -5,6 +5,7 @@ int main()
     int a, b;
     printf("Saisissez successivement deux entiers : ");
     scanf("%d %d",&a,&b);
+    printf("Juste une second pour le résultat de la somme de te deux entiers\n");
     printf("\n %d + %d = %d",a,b,(a+b));
     return 0;
 }
